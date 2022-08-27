fa347f4f51ac4276f94538ad0b3b4fd2042ba0a8
Kanik Goyal <63117613+7999999166@users.noreply.github.com>
2022-08-28 03:47:04 +0530

    jimao

--- src/db.js
+++ src/db.js
@@ -4,5 +4,5 @@ import {Dexie} from 'dexie';
       export var db = new Dexie('myDatabase');
        db.version(1).stores({friends: '++id, name, age'});
 
-        export var db5 = new Dexie('myDatabase');
+        export var db5 = new Dexie('myrata');
         db5.version(1).stores({vat: '++id, nubs'});
